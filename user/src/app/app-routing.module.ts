38bc5a64c4f5d02d84c16a92c489a7610869d49e
manuelherreragonzalez <manuel.herrera-gonzalez@capgemini.com>
2023-04-19 12:53:06 +0200

    modificaciones details

--- user/src/app/app-routing.module.ts
+++ user/src/app/app-routing.module.ts
@@ -14,7 +14,7 @@ const routes: Routes = [
     component: UserAddComponent 
   },
   { 
-    path: 'details/:user', 
+    path: 'details/:id', 
     component: UserDetailsComponent 
   }
 
--- user/src/app/components/user-details/user-details.component.ts
+++ user/src/app/components/user-details/user-details.component.ts
@@ -1,35 +1,39 @@
 import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { User } from 'src/app/models/user';
 import { UserService } from 'src/app/services/user.service';
 
 @Component({
   selector: 'app-user-details',
   templateUrl: './user-details.component.html',
-  styleUrls: ['./user-details.component.scss']
+  styleUrls: ['./user-details.component.scss'],
 })
 export class UserDetailsComponent implements OnInit {
-
   user: User = new User();
+  sub: any;
 
-  constructor(private router: Router, private userService: UserService) {
-
-  }
+  constructor(
+    private router: Router,
+    private route: ActivatedRoute,
+    private userService: UserService
+  ) {}
 
   updateUser(): void {
-    this.userService.updateUser(this.user)
-      .subscribe(data => {
-        alert("Usuario modificado de forma correcta.");
-      });
+    this.userService.updateUser(this.user).subscribe((data) => {
+      alert('Usuario modificado de forma correcta.');
+    });
+  }
 
-  };
-
-  deleteUser(user:User): void {
-    this.userService.deleteUser(user)
-  };
-
+  deleteUser(user: User): void {
+    this.userService.deleteUser(user);
+  }
 
   ngOnInit(): void {
-
+   this.sub =this.route.params.subscribe((params) => {
+      const id = params['id'];
+      this.userService.detailsUser(id).subscribe((user: any) => {
+        this.user = user;
+      });
+    });
   }
 }
